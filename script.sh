23c2a64b14fbda67c26c76d0e58c897b794564c1
John Doe <t.landes84@gmail.com>
2024-05-06 08:38:05 +0000

    Bash and linux

---
+++ script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+my_variable="hello world"
+echo $my_variable
---
+++ webscraping.py
@@ -0,0 +1,8 @@
+import time
+i = 0
+while i < (60 * 20):
+    fichier = open("data.txt", "a")
+    fichier.write(str(i) + "\n")
+    fichier.close()
+    i += 1
+    time.sleep(1)
